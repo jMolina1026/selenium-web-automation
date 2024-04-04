c4a74d96d041c4075e4fbea8470f0de3b4cea5c6
JMolina1026 <jorge.molina1026@gmail.com>
2024-04-04 15:43:32 -0700

    refactor(loginPage): corrected error

--- src/main/java/pages/LoginPage/LoginPageFinalVariables.java
+++ src/main/java/pages/LoginPage/LoginPageFinalVariables.java
@@ -7,5 +7,5 @@ public interface LoginPageFinalVariables {
 	String INVALID_PASSWORD = "WrongPassword";
 	String ERROR_MESSAGE_TEXT = "Epic sadface: Username and password do not match any user in this service";
 	String REQUIRED_USERNAME_TEXT = "Epic sadface: Username is required";
-	String REQUIRED_PASSWORD_TEXT = "Epics sadface: Password is required";
+	String REQUIRED_PASSWORD_TEXT = "Epic sadface: Password is required";
 }
